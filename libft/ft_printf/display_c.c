c62f1700ded920a84e08a49284cfb7f57ce10539
Oleksandr KRYZHANOVSKYI <okryzhan@e2r11p13.unit.ua>
2018-12-24 08:33:03 +0200

    .

---
+++ libft/ft_printf/display_c.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   display_c.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: okryzhan <okryzhan@student.unit.ua>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/13 08:43:27 by okryzhan          #+#    #+#             */
+/*   Updated: 2018/11/13 08:43:27 by okryzhan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int				display_c(t_arg *arg, va_list ap, int percent)
+{
+	int				i;
+	unsigned char	c;
+
+	i = 0;
+	c = percent ? '%' : (unsigned char)va_arg(ap, int);
+	if (arg->width && !arg->left_adj)
+		padding(arg, arg->zero_pad ? '0' : ' ', 1);
+	write(1, &c, 1);
+	if (arg->width && arg->left_adj)
+		padding(arg, ' ', 1);
+	return (arg->width > 0 ? arg->width : 1);
+}
